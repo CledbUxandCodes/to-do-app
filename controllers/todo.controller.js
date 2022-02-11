47f2e95a9b3c273eab636a8a5364523874f515e2
Cledb Astudillo <cledb.uxcodes@gmail.com>
2022-02-11 11:45:23 +0000

    Added the controller action for adding a todo item.

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -13,7 +13,26 @@ async const getAllTodos = (req, res, next) => {
     });
 };
 
-const addTodo = (req, res, next) => {};
+async const addTodo = (req, res, next) => {
+    const todoText = req.body.text;
+
+    const todo = new Todo(todoText);
+
+    let insertedId;
+    try {
+        const result = await todo.save();
+        insertedId = result.insertedId;
+    } catch (error) {
+        return next(error);
+    }
+
+    todo.id = insertedId.toString();
+
+    res.json({
+        message: 'Added todo successfully!',
+        createdTodo: todo
+    });
+};
 
 const updateTodo = (req, res, next) => {};
 
