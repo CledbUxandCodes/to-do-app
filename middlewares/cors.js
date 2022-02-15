17e816df84be92d54254a2ca5427ce378f569ca1
Cledb Astudillo <cledb.uxcodes@gmail.com>
2022-02-15 10:15:30 +0000

    Completed this to-do-API

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,8 @@
 function enableCors(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
-    res.setHeader('Access-Contol-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
-};
+}
 
 module.exports = enableCors;
